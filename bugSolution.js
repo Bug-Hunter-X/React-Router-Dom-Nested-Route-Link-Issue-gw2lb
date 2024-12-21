3a3ddd2313cb05df489a8ddb28b927e251e2c5bf
BugHunter-X <askzenai@gmail.com>
2024-12-21 09:12:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,34 @@
+```javascript
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* ... other routes ... */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link to="/about">Go to About</Link>
+    </div>
+  );
+}
+
+function About() {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>About Page</h1>
+      <button onClick={() => navigate('/')}>Go to Home</button>
+    </div>
+  );
+}
+```
